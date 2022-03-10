4d4342a31bbee35ca42fbddf1a449b5e82c96811
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-03-10 12:03:32 +0000

    Added Cart Button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     //In order to pick class for css, if css class name has "-" then class name cannot be picked with . notation
     return <Fragment>
         <header className={classes.header}>
             <h1>DelphiMealz</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="Tasty Meals" />
