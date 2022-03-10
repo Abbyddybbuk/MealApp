399b7d513c644ffdb601e00a129a92a6e3ccfaa5
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-03-10 11:49:57 +0000

    Added Header Section

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    //In order to pick class for css, if css class name has "-" then class name cannot be picked with . notation
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>DelphiMealz</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="Tasty Meals" />
+        </div>
+    </Fragment>
+};
+
+export default Header;
